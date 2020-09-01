37251ccf94b241488ce9279b0aff7c3d92360c10
WillersonAbreu <willersonabreu@hotmail.com>
2020-08-31 23:00:23 -0300

    Buscando endereço pelo CEP

---
+++ assets/js/cadastro-usuario.js
@@ -0,0 +1,50 @@
+async function getCep(valor) {
+  var cep = valor.replace(/\D/g, "");
+
+  if (cep != "") {
+    var validaCep = /^[0-9]{8}$/;
+    if (validaCep.test(cep)) {
+      try {
+        const axiosConfig = {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          responseType: "json",
+          crossDomain: true,
+        };
+
+        const { data } = await axios.get(
+          `https://ws.apicep.com/cep/${cep}.json`,
+          null,
+          axiosConfig
+        );
+
+        console.log(data);
+
+        if (data.status !== 404 && data.status !== 500) {
+          setAddressSearchValues(data);
+        } else {
+          alert("Falha ao encontrar o endereço");
+          clearForm();
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+}
+
+function setAddressSearchValues(values) {
+  document.getElementById("city").value = values.city;
+  document.getElementById("street").value = values.address;
+  document.getElementById("neighborhood").value = values.district;
+}
+
+function clearForm() {
+  document.getElementById("cep").value = "";
+  document.getElementById("city").value = "";
+  document.getElementById("street").value = "";
+  document.getElementById("neighborhood").value = "";
+}
